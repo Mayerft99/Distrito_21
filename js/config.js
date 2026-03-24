f6353de59441356323d2350bbc70abb04dd12009
Mayerft99 <carlosandradebarron@gmail.com>
2026-03-23 21:11:09 -0400

    Refactor Supabase configuration headers

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
+headers: {
+  apikey: window.SUPABASE_URL = 'https://qhpyclwnddylziambbsj.supabase.co',
+  Authorization: `Bearer ${window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFocHljbHduZGR5bHppYW1iYnNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMzI1NzAsImV4cCI6MjA4OTgwODU3MH0._JEYSQcOhg9TFhhVR_O6lYwMXhUFxj5Osx4PEFPKsQ0'
+}`,
+  'Content-Type': 'application/json'
+}
 
-window.SUPABASE_URL = 'https://qhpyclwnddylziambbsj.supabase.co';
-window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFocHljbHduZGR5bHppYW1iYnNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMzI1NzAsImV4cCI6MjA4OTgwODU3MH0._JEYSQcOhg9TFhhVR_O6lYwMXhUFxj5Osx4PEFPKsQ0';
