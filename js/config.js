6a500b559211c9318bee888affdddcb64638080a
Mayerft99 <carlosandradebarron@gmail.com>
2026-03-23 20:20:19 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+
+const SUPABASE_URL = 'https://qhpyclwnddylziambbsj.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFocHljbHduZGR5bHppYW1iYnNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyMzI1NzAsImV4cCI6MjA4OTgwODU3MH0._JEYSQcOhg9TFhhVR_O6lYwMXhUFxj5Osx4PEFPKsQ0';
